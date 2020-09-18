cd7547fc6c1d0535c466ea69436db624908a33b2
Abhijit Brahme <aub1@rice.edu>
2020-09-18 01:30:39 -0500

    now computes gravity at both basket locations

--- gravity.py
+++ gravity.py
@@ -2,14 +2,15 @@ from utils.spatial_utils import *
 from scipy.optimize import lsq_linear
 
 
-def equilibrium_matrix(frame: np.ndarray, basket: np.ndarray = np.array([[25.0, 4.0]])) -> np.ndarray:
+def equilibrium_matrix(frame: np.ndarray, basket: np.ndarray = np.array([[4.0, 25.0], [90.0, 25.0]])) -> np.ndarray:
     """
     sets up equilibrium equation for x and y
-    :param frame: (11 x 5) array of one frame already corrected for location
+    :param frame: (11 x 5) array of one frame already corrected for location,
+    assumes first entry is the ball
     :param basket: location of basket (1 x 2) array
     :return:
     """
-    A = np.zeros((24, 12))
+    A = np.zeros((26, 13))
 
     # append basket to frame
     locations = np.vstack((frame[:, 2:4], basket))
@@ -17,12 +18,12 @@ def equilibrium_matrix(frame: np.ndarray, basket: np.ndarray = np.array([[25.0, 
     distance_weight = squared_distance(locations, locations)
 
     # begin jackknife
-    for i in range(12):
+    for i in range(13):
         jackknifed_position = center(locations, locations[i, :])
         # jackknifed position
         if i != 0:  # ball
             # rotate to align ball with + x-axis
-            rotation_angle = np.arctan2(locations[0, 1], locations[0, 0])
+            rotation_angle = np.arctan2(jackknifed_position[0, 1], jackknifed_position[0, 0])
             if rotation_angle < 0:
                 rotation_angle *= -1
                 rotation_angle += np.pi / 2
@@ -35,24 +36,24 @@ def equilibrium_matrix(frame: np.ndarray, basket: np.ndarray = np.array([[25.0, 
         sin_theta = rotated_jackknife[:, 1] / norms
         x_row = 2 * i
         y_row = 2 * i + 1
-        for j in range(12):
+        for j in range(13):
             if i != j:
                 A[x_row, j] = (1 / distance_weight[i, j]) * cos_theta[j]
                 A[y_row, j] = (1 / distance_weight[i, j]) * sin_theta[j]
-    return A
+
+    return np.nan_to_num(A)
 
 
 def calculate_gravity(frame: np.ndarray) -> np.ndarray:
     """
     solves the equilibrium equation using least squares with constraints
-    :param frame:
+    :param frame: (11 x 5) array
     :return:
     """
     A = equilibrium_matrix(frame)
-    lb = -np.inf * np.ones((12,))
+    lb = -np.inf * np.ones((13,))
     lb[0] = 1  # sets the ball to be 1
-    ub = np.inf * np.ones((12,))
+    ub = np.inf * np.ones((13,))
     ub[0] = 1.01
-    result = lsq_linear(A, np.zeros((24,)), bounds=(lb, ub))
+    result = lsq_linear(A, np.zeros((26,)), bounds=(lb, ub))
     return result.x
-
