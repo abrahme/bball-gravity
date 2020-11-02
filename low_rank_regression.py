2d59a715e83f5084e7084e685db3f2a52cfc55a5
Abhijit Brahme <aub1@rice.edu>
2020-11-01 22:14:09 -0600

    added command line flexibility

--- low_rank_regression.py
+++ low_rank_regression.py
@@ -4,16 +4,43 @@ from torchvision import transforms
 from torch.utils.data import DataLoader
 from model.Tensor import *
 import torch.optim as optim
+import argparse
+import yaml
+
+
+def read_args(arg_path: str):
+    """
+
+    :param arg_path: where to get the arguments from
+    :return:
+    """
+    with open(arg_path, "r") as f:
+        arg_yaml = yaml.load(f)
+    f.close()
+    parser = argparse.ArgumentParser(description='Train gravity tensor model')
+    for arg in arg_yaml:
+        name = f"--{arg['name']}"
+        dest = arg["dest"]
+        help_desc = arg["help"]
+        default_val = arg["default"]
+        action = arg["action"]
+        parser.add_argument(name, dest=dest, action=action, default=default_val, help=help_desc, required=False)
+    return parser.parse_args()
+
 
 if __name__ == "__main__":
-    tl.set_backend("pytorch")
-    batch_size = 8
-    device = "cpu"
 
-    player_map = read_json("processed_data/player_map.json")
+    parsed_args = read_args("configs/args.yaml")
+    player_file = parsed_args.player_data_file
+    backend = parsed_args.backend
+    tl.set_backend(backend)
+    player_map = read_json(player_file)
     num_players = len(player_map)
-    x_res = 24
-    y_res = 25
+    x_res = parsed_args.Q
+    y_res = parsed_args.R
+    batch_size = parsed_args.batch_size
+    device = parsed_args.device
+    lr = parsed_args.learning_rate
 
     dataset = GravityDataSet(T=960, P=num_players, Q=x_res, R=y_res, max_x=47, max_y=50,
                              data_dir_globbed="processed_data/possession_data/*.pkl",
@@ -23,12 +50,12 @@ if __name__ == "__main__":
     train_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
     model = CPRL(input_size=(batch_size, 960, 2 * num_players, x_res, y_res),
-                 rank=2, output_size=(batch_size, 1))
+                 rank=1, output_size=(batch_size, 1))
 
-    optimizer = optim.SGD(model.parameters(), lr=.01, momentum=.9)
+    optimizer = optim.SGD(model.parameters(), lr=lr, momentum=.9)
     criterion = nn.MSELoss()
-    n_epochs = 1
-    regularizer = .01
+    n_epochs = parsed_args.n_epochs
+    regularizer = parsed_args.regularizer
     model = model.to(device)
 
     model.train()
